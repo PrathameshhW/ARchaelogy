efba88d3fead9f01a3845d18e00b41307643d00e
Prathamesh Jadhav <prathameshj0407@gmail.com>
2024-12-30 22:45:56 +0530

    feat: Initial Header setup

--- src/app/MainContainer.tsx
+++ src/app/MainContainer.tsx
@@ -2,11 +2,19 @@
 
 import { NextUIProvider } from "@nextui-org/react";
 import { Provider as JotaiProvider } from "jotai";
+import { useRouter } from "next/navigation";
 import React from "react";
+import Header from "./components/Header";
+
 const ManContainer = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+
   return (
-    <NextUIProvider>
-      <JotaiProvider>{children}</JotaiProvider>
+    <NextUIProvider navigate={router.push}>
+      <JotaiProvider>
+        <Header />
+        {children}
+      </JotaiProvider>
     </NextUIProvider>
   );
 };
